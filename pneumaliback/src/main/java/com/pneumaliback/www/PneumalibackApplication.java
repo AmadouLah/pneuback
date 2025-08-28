ecb11bfee783d93d69aa9b98bbd72825fe804bfc
Amadou Landoure <amadoulandoure004@gmail.com>
2025-08-28 19:58:30 +0000

    first commit

---
+++ pneumaliback/src/main/java/com/pneumaliback/www/PneumalibackApplication.java
@@ -0,0 +1,13 @@
+package com.pneumaliback.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PneumalibackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PneumalibackApplication.class, args);
+	}
+
+}
